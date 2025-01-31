1d9118b090d5220194cac1e3d9e938de0be7e071
gabezolla <gabriel.zolla1998@gmail.com>
2025-01-31 02:43:08 -0300

    include cqrs strategy on application layer

---
+++ LeagueStats.Application/CommandHandlers/GetSummonerCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using LeagueStats.Application.Commands;
+using LeagueStats.Domain.Entities;
+using LeagueStats.Infrastructure.Services;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeagueStats.Application.CommandHandlers
+{
+    public class GetSummonerCommandHandler : IRequestHandler<GetSummonerCommand, Summoner> // TODO: change to SummonerViewModel
+    {
+        private readonly IAccountService _accountService;
+
+        public GetSummonerCommandHandler(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        public async Task<Summoner> Handle(GetSummonerCommand request, CancellationToken cancellationToken)
+        {
+            return await _accountService.GetSummoner(request.GameName, request.TagLine);            
+        }
+    }
+}
---
+++ LeagueStats.Application/Commands/GetSummonerCommand.cs
@@ -0,0 +1,19 @@
+﻿using LeagueStats.Domain.Entities;
+using MediatR;
+
+namespace LeagueStats.Application.Commands
+{
+    public class GetSummonerCommand : IRequest<Summoner> // TODO: Change to SummonerViewModel
+    {
+        public GetSummonerCommand(string gameName, string tagLine)
+        {
+            GameName = gameName;
+            TagLine = tagLine;
+        }
+
+        public string GameName { get; set; }
+
+        public string TagLine { get; set; }
+
+    }
+}
