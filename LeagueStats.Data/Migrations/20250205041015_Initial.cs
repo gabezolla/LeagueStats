c2b7f934f75ea75d5d1e8d110adaf9b0c3090e95
gabezolla <gabriel.zolla1998@gmail.com>
2025-02-05 01:11:02 -0300

    add migrations

---
+++ LeagueStats.Data/Migrations/20250205041015_Initial.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LeagueStats.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Stats",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(100)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    GameName = table.Column<string>(type: "varchar(100)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    PlayerId = table.Column<string>(type: "varchar(100)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Champion = table.Column<string>(type: "varchar(100)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Lane = table.Column<string>(type: "varchar(100)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    DamageDealt = table.Column<double>(type: "double", nullable: false),
+                    DamageTaken = table.Column<double>(type: "double", nullable: false),
+                    StealthWardsPlaced = table.Column<int>(type: "int", nullable: false),
+                    TeamDamagePercentage = table.Column<double>(type: "double", nullable: false),
+                    DamagePerMinute = table.Column<double>(type: "double", nullable: false),
+                    SkillshotsDodged = table.Column<int>(type: "int", nullable: false),
+                    SkillshotsHit = table.Column<int>(type: "int", nullable: false),
+                    Kda = table.Column<double>(type: "double", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stats", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Summoners",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(100)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    GameName = table.Column<string>(type: "varchar(100)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Tagline = table.Column<string>(type: "varchar(100)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Summoners", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Stats");
+
+            migrationBuilder.DropTable(
+                name: "Summoners");
+        }
+    }
+}
