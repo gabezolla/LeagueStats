ae1fba2978ca611c5174cf1083296685e156bcc8
gabezolla <gabriel.zolla1998@gmail.com>
2025-02-05 00:44:44 -0300

    implement method to retrieve stored stats from database

---
+++ LeagueStats.Application/CommandHandlers/GetStoredStatsFromPlayerCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using LeagueStats.Application.Commands;
+using LeagueStats.Data.Repository;
+using LeagueStats.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeagueStats.Application.CommandHandlers
+{
+    public class GetStoredStatsFromPlayerCommandHandler : IRequestHandler<GetStoredStatsFromPlayerCommand, IEnumerable<Stats>>
+    {
+        private readonly IMatchStatsRepository _repository;
+
+        public GetStoredStatsFromPlayerCommandHandler(IMatchStatsRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IEnumerable<Stats>> Handle(GetStoredStatsFromPlayerCommand request, CancellationToken cancellationToken)
+        {
+            return await _repository.GetPlayerStatsBySummonerId(request.PlayerId);
+        }
+    }
+}
---
+++ LeagueStats.Application/Commands/GetStoredStatsFromPlayerCommand.cs
@@ -0,0 +1,20 @@
+using LeagueStats.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeagueStats.Application.Commands
+{
+    public class GetStoredStatsFromPlayerCommand : IRequest<IEnumerable<Stats>>
+    {
+        public GetStoredStatsFromPlayerCommand(string playerId)
+        {
+            PlayerId = pla;
+        }
+
+        public string PlayerId { get; set; }
+    }
+}
