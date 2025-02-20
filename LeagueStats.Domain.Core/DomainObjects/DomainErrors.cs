7f6c05b7585f0ae680b62158319102a1d9951b2a
gabezolla <gabriel.zolla1998@gmail.com>
2025-02-19 22:46:45 -0300

    add domain errors

---
+++ LeagueStats.Domain.Core/DomainObjects/DomainErrors.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeagueStats.Domain.Core.DomainObjects
+{
+    public class DomainErrors
+    {
+        public const string InvalidCredentials = "Invalid credentials";
+    }
+}
