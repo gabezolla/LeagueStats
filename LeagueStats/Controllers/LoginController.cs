36866da83b493db7c2a8773a8a6008d3df651944
gabezolla <gabriel.zolla1998@gmail.com>
2025-02-19 22:41:42 -0300

    implement login controller

---
+++ LeagueStats/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using LeagueStats.Application.Commands;
+using LeagueStats.Application.Models;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace LeagueStats.Controllers
+{
+    [Route("api/v1/login")]
+    public class LoginController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public LoginController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] LoginRequest request)
+        {
+            try
+            {
+                var token = await _mediator.Send(new LoginCommand(request));
+
+                return Ok(token);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.BadRequest, ex.Message);            
+            }
+        }
+    }
+}
