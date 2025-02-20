728b993123ebed24c36b07da87b427a8e532886f
gabezolla <gabriel.zolla1998@gmail.com>
2025-02-19 22:42:59 -0300

    implement jwt provider

---
+++ LeagueStats.Infrastructure/Authentication/JwtProvider.cs
@@ -0,0 +1,46 @@
+﻿using LeagueStats.Application.Abstractions;
+using LeagueStats.Domain.Entities;
+using LeagueStats.Infrastructure.Configurations;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Pomelo.EntityFrameworkCore.MySql.Query.ExpressionVisitors.Internal;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeagueStats.Infrastructure.Authentication
+{
+    public class JwtProvider : IJwtProvider
+    {
+        private readonly JwtSettings _jwtSettings;
+        private readonly DateTime _expiration = DateTime.UtcNow.AddHours(1);
+
+        public JwtProvider(IOptions<JwtSettings> jwtSettings)
+        {
+            _jwtSettings = jwtSettings.Value;
+        }
+
+        public string GenerateToken(Member member)
+        {
+            var claims = new Claim[]
+            {
+                new(JwtRegisteredClaimNames.Sub, member.Id),
+                new (JwtRegisteredClaimNames.Email, member.Email)
+            };
+
+            var signInCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.Key)), 
+                SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_jwtSettings.Issuer, _jwtSettings.Audience, claims, null, _expiration, signInCredentials);
+
+            var tokenValue = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return tokenValue;
+        }
+    }
+}
---
+++ LeagueStats.Infrastructure/Configurations/JwtSettings.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeagueStats.Infrastructure.Configurations
+{
+    public class JwtSettings
+    {
+        public string Issuer { get; init; }
+
+        public string Audience { get; init; }
+
+        public string Key { get; init; }
+    }
+}
